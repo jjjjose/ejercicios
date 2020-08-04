b47330de9c49940ff72ffdf89192ccbd716acb8d
jjjjose <jjjjoseignacio@gmail.com>
2020-08-04 00:30:20 -0400

    .

---
+++ script.js
@@ -0,0 +1,59 @@
+//este es mi primer codigo en javascript
+/*
+esto
+es
+un
+comentario
+multilinea
+*/
+// let persona = {
+//   nombre: "Jose Ignacio",
+//   email: "asdfsad@puto.com",
+// };
+
+// let personal = [
+//   {
+//     nombre: "luis alberto",
+//     posicion: "portero",
+//     edad: 105,
+//     activo: true,
+//   },
+//   {
+//     nombre: "Ana Maria",
+//     posicion: "Secretaria",
+//     edad: 15,
+//     activo: false,
+//   },
+// ];
+
+// const nombre = "Jose Ignacio T. A.";
+// const cajon2 = 10;
+// var edad = 50;
+// let otroCajon = "otra cosa";
+// var cAjon = `una cosa`;
+// let cajonsito = true;
+// let objeto = {
+//   objeto1: 10,
+//   objeto2: false,
+//   objeto3: "hola",
+// };
+// let array = [
+//   {
+//     uno: 1,
+//   },
+//   {
+//     dos: false,
+//   },
+//   {
+//     tres: "string",
+//   },
+// ];
+
+// declarando mis variables
+var nombres = "Jose Ignacio ";
+const apellidos = "Ticona Acarapi A";
+let EDAD = 45;
+
+console.warn("Mis primeras variables");
+
+console.log(nombres + apellidos + " edad: " + EDAD + " años");
